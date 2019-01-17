fbd7d701bfb48ce143c34952ae95987463288c43
Ewar1694 <46724872+Ewar1694@users.noreply.github.com>
2019-01-17 14:49:32 -0500

    Third project

---
+++ M2T1_warren/main.cpp
@@ -0,0 +1,72 @@
+// CSC 134
+// M2T1
+// E Warren
+// 1/17/18
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Listings 3-1 to 3-9
+    cout << "Listing 3-1" << endl;
+    int mynumber;
+    mynumber = 10;
+    cout << mynumber << endl;
+
+    cout << "Listing 3-2" << endl;
+    mynumber = 20;
+    cout << mynumber << endl;
+
+    cout << "Listing 3-3" << endl;
+    int start;
+    int time;
+    start = 37;
+    time = 22;
+    cout << start + time << endl;
+
+    cout << "Listing 3-4" << endl;
+    int final;
+    final = 28;
+    time = 18;
+    cout << final - time << endl;
+
+    cout << "Listing 3-5" << endl;
+    int first, second;
+    cout << "Dividing 28 by 14." << endl;
+    first = 28;
+    second = 14;
+    cout << "Quotient: " << first / second << endl;
+    cout << "Remainder: " << first % second << endl;
+    cout << "Dividing 32 by 6." << endl;
+    first = 32;
+    second = 6;
+    cout << "Quotient: " << first / second << endl;
+    cout << "Remainder: " << first % second << endl;
+
+    cout << "Listing 3-6" << endl;
+    string mystring;
+    mystring = "abcdef";
+    cout << mystring[2] << endl;
+
+    cout << "Listing 3-7" << endl;
+    mystring = "Hello there!";
+    cout << mystring << endl;
+
+    cout << "Listing 3-8" << endl;
+    first = 10;
+    second = 20;
+    string result;
+    result = first == second ? "equal" : "not equal";
+    cout << result << endl;
+
+    cout << "Listing 3-9" << endl;
+    string name;
+    cout << "Type your name: ";
+    cin >> name;
+    cout << "Your name is " << name << endl;
+
+    //only one return
+    return 0;
+}
