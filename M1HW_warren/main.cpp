efee0bc2f36ab75facd512463d6cfbd0b0bebd83
Ewar1694 <46724872+Ewar1694@users.noreply.github.com>
2019-01-17 14:08:31 -0500

    Add files via upload
    Second upload, my holy message to you mortals

---
+++ M1HW_warren/main.cpp
@@ -0,0 +1,26 @@
+// CSC 134
+//M1H1
+//E Warren
+//1/17/19
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Introduce thyself
+
+    cout << "Hello world!" << endl;
+    cout << "The world was blessed with my presence upon the holy day of February " << 23 << ", " << 1999 << ", a day made great by my newly formed presence!" <<endl;
+    cout << "My greatness hath been foretold, as I was the chosen one, the great one, the very being who would transcend humanity!" << endl;
+    cout << "I know this, for only a deity like myself could think of an idea as such the one I've had, an idea which enveloped my very soul and mind!" << endl;
+    cout << "I was handpicked by the celestial beings so far beyond our understanding that we could not even imagine them, and endowed with great knowledge that will bring humanity upon the cusp of perfection!" << endl;
+    cout << "Ah, but mortal man was not ready! They deny my holy quest, but they will all one day listen to me!" << endl;
+    cout << "Even if it takes " << 100 << " years, " << 1000 << ", even " << 1000000 << ", they will all one day accept me and my message from above!" << endl;
+    cout << "Turn your mortal eyes, for once you behold the knowledge of the gods, there is no going back!" << endl;
+    cout << "Are you truly prepared?!? Then listen! HEAR ME NOW, AND THE INFINITE WISDOM OF THOSE BILLIONS OF YEARS BEYOND OUR EXISTANCE!" << endl;
+    cout << "GIF IS PRONOUNCED WITH A HARD G!" << endl;
+    cout << "The gods and I do NOT care what Steve Wilhite says, he is WRONG and CTHULU TOLD ME!" << endl;
+    return 0;
+}
